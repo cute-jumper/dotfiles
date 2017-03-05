612360debdd7b24997c3330402fbc814135804d0
Junpeng Qiu <qjpchmail@gmail.com>
2017-03-04 22:46:28 -0500

    Change setup script to work with stow

--- setup.sh
+++ setup.sh
@@ -5,28 +5,9 @@
 # Description: Setup environment
 
 set -e
-DIR=$HOME/.dotfiles
 
-echo -e "\e[0;32mSTEP 1:\e[m"
-FILES=(.latexmkrc .hunspell_en_US .gitconfig .keysnail.js .vimrc .ideavimrc)
-echo "Would you like to create following symbol links?"
-counter=1
-for i in "${FILES[@]}"; do
-    echo "$counter: $HOME/$i"
-    counter=$(($counter+1))
-done
-while true; do
-    read -p "Your answer: [Y/N] " yn
-    case $yn in
-        [Yy]* )
-            for i in ${FILES[@]}
-            do
-                (set -x; ln -si $DIR/$i $HOME/$i)
-            done
-            break;;
-        [Nn]* )
-            echo "You choose to do nothing."
-            break;;
-        * ) echo "Please answer Y or N.";;
-    esac
+MODULES=(latexmk hunspell git keysnail vim ideavim)
+for i in "${MODULES[@]}"
+do
+    stow -v $i
 done
